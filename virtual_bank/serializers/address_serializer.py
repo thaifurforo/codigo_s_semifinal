905f084bf87ce00c805609ddb1b36448fa6a40ac
Thainara Furforo <thaifurforo@gmail.com>
2022-07-09 15:08:20 -0300

    Implemented full address on customers' GET request response
    Implemented:
    - Costumer
    - Account including Balance on GET
    - Transactions affecting Balance
    - Balance (not serialized)
    - Addresses (not serialized)
    - ListAPIView - all accounts for each costumer / all transactions for each account
    - Import full Address from Correios API by Zipcode
    - Add full Address to customer GET request response
    
    Yet to be implemented:
    - Filters in the Viewsets
    - Some validators
    - Tests
    - Script documentation
    - Readme documentation

---
+++ virtual_bank/serializers/address_serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from virtual_bank.models.address_model import Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Address
+        fields = ('city', 'district', 'neighborhood', 'street')
--- virtual_bank/serializers/customer_serializer.py
+++ virtual_bank/serializers/customer_serializer.py
@@ -1,40 +1,78 @@
 from rest_framework import serializers
 from virtual_bank.models import Customer
+from virtual_bank.models.address_model import Address
+from virtual_bank.serializers.address_serializer import AddressSerializer
 from virtual_bank.validators import *
 
+
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
-  class Meta():
-    model = Customer
-    fields = '__all__'
+    city = serializers.SerializerMethodField()
 
-  def validate(self, data):
-    """Valida se os dados preenchidos em cada campo estão de acordo com os formatos adequados"""
+    def get_city(self, obj):
+        address_queryset = Address.objects.filter(zip_code=obj.zip_code)
+        address_serializer = AddressSerializer(address_queryset, many=True)
+        return address_serializer.data[0]['city']
 
-    if not document_number_numbers_only_validate(data['document_number']):
-      raise serializers.ValidationError({'document_number':'O parâmetro "document_number" deve ser um integer ou uma string com somente números'})
+    district = serializers.SerializerMethodField()
 
+    def get_district(self, obj):
+        address_queryset = Address.objects.filter(zip_code=obj.zip_code)
+        address_serializer = AddressSerializer(address_queryset, many=True)
+        return address_serializer.data[0]['district']
 
-    if data['client_type'] == 'PF':
+    neighborhood = serializers.SerializerMethodField()
 
-      if not cpf_length_validate(data['document_number']):
-        raise serializers.ValidationError({'document_number':'O CPF deve ter 11 dígitos'})
+    def get_neighborhood(self, obj):
+        address_queryset = Address.objects.filter(zip_code=obj.zip_code)
+        address_serializer = AddressSerializer(address_queryset, many=True)
+        return address_serializer.data[0]['neighborhood']
 
-      if not cpf_check_digit_validate(data['document_number']):
-        raise serializers.ValidationError({'document_number':'CPF inválido'})
+    street = serializers.SerializerMethodField()
 
-      if not birthdate_if_pf_validate(data['birthdate']):
-        raise serializers.ValidationError({'birthdate':'Obrigatório preencher Data de Nascimento para Pessoas Físicas'})
+    def get_street(self, obj):
+        address_queryset = Address.objects.filter(zip_code=obj.zip_code)
+        address_serializer = AddressSerializer(address_queryset, many=True)
+        return address_serializer.data[0]['street']
 
-    
-    if data['client_type'] == 'PJ':
+    class Meta():
+        model = Customer
+        fields = ('url', 'client_type', 'document_number', 'name', 'phone_number', 'email', 'birthdate',
+                  'zip_code', 'city', 'district', 'neighborhood', 'street', 'door_number', 'complement')
+        depth = 1
 
-      if not cnpj_length_validate(data['document_number']):
-        raise serializers.ValidationError({'document_number':'O CNPJ deve ter 14 dígitos'})
+    def validate(self, data):
+        """Valida se os dados preenchidos em cada campo estão de acordo com os formatos adequados"""
 
-      if not cnpj_check_digit_validate(data['document_number']):
-        raise serializers.ValidationError({'document_number':'CNPJ inválido'})
+        if not document_number_numbers_only_validate(data['document_number']):
+            raise serializers.ValidationError(
+                {'document_number': 'O parâmetro "document_number" deve ser um integer ou uma string com somente números'})
 
-      if not not_birthdate_if_pj_validate(data['birthdate']):
-        raise serializers.ValidationError({'birthdate':'Pessoa Jurídica não deve ter Data de Nascimento'})
+        if data['client_type'] == 'PF':
 
-    return data
+            if not cpf_length_validate(data['document_number']):
+                raise serializers.ValidationError(
+                    {'document_number': 'O CPF deve ter 11 dígitos'})
+
+            if not cpf_check_digit_validate(data['document_number']):
+                raise serializers.ValidationError(
+                    {'document_number': 'CPF inválido'})
+
+            if not birthdate_if_pf_validate(data['birthdate']):
+                raise serializers.ValidationError(
+                    {'birthdate': 'Obrigatório preencher Data de Nascimento para Pessoas Físicas'})
+
+        if data['client_type'] == 'PJ':
+
+            if not cnpj_length_validate(data['document_number']):
+                raise serializers.ValidationError(
+                    {'document_number': 'O CNPJ deve ter 14 dígitos'})
+
+            if not cnpj_check_digit_validate(data['document_number']):
+                raise serializers.ValidationError(
+                    {'document_number': 'CNPJ inválido'})
+
+            if not not_birthdate_if_pj_validate(data['birthdate']):
+                raise serializers.ValidationError(
+                    {'birthdate': 'Pessoa Jurídica não deve ter Data de Nascimento'})
+
+        return data
