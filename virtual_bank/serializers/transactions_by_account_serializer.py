578fc36ed2117715b3fcb5d04a67b2b8df742ff2
Thainara Furforo <thaifurforo@gmail.com>
2022-07-09 11:41:48 -0300

    Update transactions_by_account_serializer.py
    Showing the description for the transaction type, instead of the initials

--- virtual_bank/serializers/transactions_by_account_serializer.py
+++ virtual_bank/serializers/transactions_by_account_serializer.py
@@ -3,6 +3,11 @@ from virtual_bank.models.transaction_model import Transaction
 
 
 class TransactionsByAccountSerializer(serializers.ModelSerializer):
+    # Returns the description for the transaction type, instead of the initials
+    transaction_type = serializers.SerializerMethodField()
+
+    def get_transaction_type(self, obj):
+        return obj.get_transaction_type_display()
 
     class Meta:
         model = Transaction
