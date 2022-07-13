bfe3676e1bee27c0ad7229a3ef49590175f4c8a2
Thainara Furforo <thaifurforo@gmail.com>
2022-07-12 23:46:07 -0300

    Documented the AccountView class and it's functions

--- virtual_bank/views/account_view.py
+++ virtual_bank/views/account_view.py
@@ -7,6 +7,32 @@ from virtual_bank.serializers import AccountSerializer
 
 
 class AccountViewSet(viewsets.ModelViewSet):
+    """This class creates a ModelViewSet for the Account model.
+
+    It allows the following http methods to be used on the requests for this view:
+    get, post put, patch, head, options.
+    Therefore, it's not possible to delete an account. It can only be turn active
+    or inactive (when closed).
+
+    The queryset determinates that the Account objects will be shown first by
+    active_accounts, then by opening_date, then by closure_date, and finally by
+    account_number.
+
+    The serializer class used for this view is the AccountSerializer
+
+    There are set some filters for this ViewSet:
+    The data can be ordered by opening_date, closure_date, custommer or customer_name.
+    The data can be searched by account_number.
+    The data can be filtered by selecting if the active_account is True or False, 
+    by selecting a opening_date (exact, greater than or equal, less than or equal),
+    by selection a closure_date (exact, greater than or equal, less than or equal),
+    and/or by selecting a specific customer.
+
+    The authentication for this ViewSet is BasicAuthentication, which means that the
+    authentication is made through an user an password combination.
+    The permission class is IsAuthenticated, which means that it alows access to
+    any authenticated user, and denies access to any unauthenticated user.
+    """
 
     http_method_names = ['get', 'post', 'put', 'patch', 'head', 'options']
 
@@ -19,12 +45,21 @@ class AccountViewSet(viewsets.ModelViewSet):
                        filters.OrderingFilter, filters.SearchFilter]
 
     ordering_fields = ['opening_date',
-                       'closure_date', 'customer', 'customer_id']
+                       'closure_date', 'customer', 'customer_name']
     search_fields = ['account_number']
     filterset_fields = {'active_account': ['exact'], 'opening_date': [
         'gte', 'lte', 'exact'],  'closure_date': ['gte', 'lte', 'exact'], 'customer': ['exact']}
 
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
     def get_serializer(self, *args, **kwargs):
+        """Overwrites the get_serializer function for this ViewSet, whcih returns
+        the serializer instance that should be used for validating and deserializing
+        input, and for serializing output.
+        Turns possible to make multiple POST requests at the same time, using an
+        array of objects in the JSON file.
+        """
         if 'data' in kwargs:
             data = kwargs['data']
 
@@ -32,6 +67,3 @@ class AccountViewSet(viewsets.ModelViewSet):
                 kwargs['many'] = True
 
         return super().get_serializer(*args, **kwargs)
-
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
